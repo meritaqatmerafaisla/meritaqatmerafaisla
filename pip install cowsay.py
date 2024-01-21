2295f2fb91afd43d700face9e18fa9726af6cd2c
Anshu Kumar Ranjan <52268442+meritaqatmerafaisla@users.noreply.github.com>
2024-01-21 14:22:21 +0530

    Update pip install cowsay.py

--- pip install cowsay.py
+++ pip install cowsay.py
@@ -1,18 +1,1 @@
-loop=1
-while (loop<10):
-    noun=input("choose a noun: ")
-    p_noun=input("choose a plural noun: ")
-    noun2=input("choose a noun: ")
-    place=input("name a place: ")
-    adjective=input("choose an adjective(describing word): ")
-    noun3=input("choose a noun: ")
-    print("----------------------------------")
-    print("Be kind to your",noun,"-footed",p_noun)
-    print("For a duck may be somebody",noun2,",")
-    print("Be kind to your",p_noun,"in",place)
-    print("Where weather is always",adjective,".")
-    print()
-    print("You may think that is the",noun3,",")
-    print("Well, It is.")
-    print("----------------------------------")
-    loop=loop+1
+
